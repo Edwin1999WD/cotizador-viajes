e57e75e46dfdb14b958dc875867c6fc1478c60ed
Edwin Daylan Esparza García <90080563+Edwin1999WD@users.noreply.github.com>
2021-12-09 12:07:06 -0600

    Add files via upload

---
+++ Trip Calculator/js/scripts.js
@@ -0,0 +1,49 @@
+
+let vacationCalc = document.getElementById('vacationCalc');
+
+vacationCalc.addEventListener('submit', calcExpenses);
+
+function calcExpenses (e) {
+
+    e.preventDefault();
+
+    let destiny = document.getElementById('destiny').value;
+    let budget = document.getElementById('budget').value;
+    let acomodation = document.getElementById('acomodation').value;
+    let transport = document.getElementById('transport').value;
+    let food = document.getElementById('food').value; 
+    
+    let expenses = parseInt(acomodation) + parseInt(transport) + parseInt(food);
+    let balance = budget - expenses;
+
+    ui(destiny,budget,balance);
+
+    
+}
+
+function ui(destiny,budget,balance) {
+    let result = document.getElementById('result');
+    let dataPrint = document.createElement('div');
+
+    dataPrint.innerHTML =  `
+      <div class='container-data row'>
+      <div class='col s4'>
+       <h6> ${destiny}</h6>
+      </div>
+      <div class='col s4'>
+      <h6> ${budget}</h6>
+     </div>
+     <div class='col s4'>
+     <h6> ${balance}</h6>
+    </div>
+      </div>     
+    `
+
+    result.appendChild(dataPrint);
+
+    reset();
+}
+
+function reset() {
+    document.getElementById('vacationCalc').reset();
+}
